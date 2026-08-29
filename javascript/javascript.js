34d41e9b1552ba7191cef3d0f7fc0103a01be781
LAMKADEMADIL <adillamkadem215@gmail.com>
2026-08-30 00:23:44 +0100

    new

---
+++ javascript/javascript.js
@@ -0,0 +1,5 @@
+ const menuBtn = document.getElementById("menuBtn");
+      const menu = document.getElementById("menu");
+
+      menuBtn.onclick = function () {
+        menu.hidden = !menu.hidden;}
